198a9c0d4579a469caf82cc1f40244854302023c
Juanrhec904 <juandavidrhenalsmontero@gmail.com>
2025-05-21 08:43:23 -0500

    Creacion vista crear
    Esta es la vista que le tiene que salir al usuario una vez que el usuario desee "crear una nueva nota" para la tarea que desee realizar

--- primera_app/lib/main.dart
+++ primera_app/lib/main.dart
@@ -13,6 +13,7 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false, 
-    home: Loandig());
+    home: sucess()
+    );
   }
 }
